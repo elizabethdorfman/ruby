dcec7335241b88bdc79147dfac188a0028b644ce
Elizabeth Dorfman <elizabethdorfman31@gmail.com>
2023-09-07 15:55:18 -0400

    chapter 2

---
+++ ch_2_vars/age.rb
@@ -0,0 +1,14 @@
+print "How old are you?"
+age= gets.chomp.to_i
+ten= age+10
+twenty= age+20
+thirty= age+30
+fourty = age+40
+puts "In 10 years you will be"
+puts ten
+puts "In 20 years you will be:"
+puts twenty
+puts "In 30 years you will be"
+puts thirty
+puts "in 40 years you will be"
+puts fourty
---
+++ ch_2_vars/name.rb
@@ -0,0 +1,6 @@
+print "Please type in your first name:"
+first_name= gets.chomp
+print "Please type in your last name:"
+last_name= gets.chomp
+print "Hello #{first_name} #{last_name} "
+10.times{ print"#{first_name}"}
